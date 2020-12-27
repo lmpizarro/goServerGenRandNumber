8f21a4fb4a7154e36de1f552f6bedcbf6fbdc734
lmpizarro <lmpizarro@gmail.com>
2020-12-27 15:07:45 -0300

    create pdf

--- pkg/randgen/main.go
+++ pkg/randgen/main.go
@@ -18,12 +18,38 @@ type StatSample struct {
 	Value float64
 }
 
+type valuePairs struct {
+	pdf []float64
+	sample []float64
+}
+
 var samples = []StatSample{{10, 20}, {3, 10}, {5, 2}}
 
 // SetSeed ...
 func SetSeed(seed int64) {
 	rand.Seed(seed)
 }
+
+var vals valuePairs
+
+func createValuePairs(samples []StatSample, N int, Nsamples int) {
+
+	vals.pdf = make([]float64, N)
+	vals.sample = make([]float64, N)
+
+	vals.sample[0] = samples[0].Value
+	vals.pdf[0] = float64(samples[0].Count) / float64(Nsamples)
+
+	for i := 1; i < N; i++ {
+		vals.pdf[i] = vals.pdf[i-1] + float64(samples[i].Count) / float64(Nsamples)
+		vals.sample[i] = samples[i].Value
+		
+	}
+
+	fmt.Println(vals.pdf, vals.sample, float64(N))
+
+}
+
 
 // CreateStatFromCSV ...
 func CreateStatFromCSV(filename string) {
@@ -81,12 +107,19 @@ func CreateStat(samples []StatSample) {
 	log.Println("CREATING STAT")
 	var array []float64
 
+	var Nvals int = 0
+	var Nsamples int = 0
 	for _, v := range samples {
+		Nvals++
+		Nsamples = Nsamples + v.Count
 		fmt.Println(v.Count, v.Value)
 		arr := Createarray(v)
 		array = append(array, arr...)
 	}
 
+	log.Printf("Nvals: %d, Nsamples %d", Nvals, Nsamples)
+	log.Println(samples)
+	createValuePairs(samples, Nvals, Nsamples)
 	samplerarray = array
 }
 
